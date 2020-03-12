5d36547908f0cb1864d4593f0314ec3c8284179f
Kristian <krismo94@hotmail.com>
2020-03-12 16:41:24 +0100

    Lagt til img modal og produkt-info

--- resources/js/components/SingleProduct.vue
+++ resources/js/components/SingleProduct.vue
@@ -1,43 +1,66 @@
 <template>
   <div>
-    <div class="container">
+    <div class="container mb-5">
       <div class="row">
-        <div
-          class="card col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <img src="img/men_hoodie.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <p class="card-brand">{{product.brand}}</p>
-              <p class="card-name">{{product.name}}</p>
-              <p class="card-price">{{product.price}},-</p>
-            </div>
+        <div class="card col-lg-4 col-md-6 col-sm-6 col-xs-12">
+          <img
+            src="img/men_hoodie.jpg"
+            class="card-img-top"
+            alt="..."
+            data-toggle="modal"
+            data-target="#imgModal"
+          />
+        </div>
+        <div class="col-lg-8 col-md-6 col-sm-6 col-xs-12">
+          <p class="sp-brand">Brand-placeholder</p>
+          <p class="sp-name">{{product.name}}</p>
+          <p class="sp-price">{{product.price}},-</p>
+          <p class="sp-details">{{product.details}}</p>
+          <p class="sp-description">{{product.description}}</p>
+        </div>
+      </div>
+    </div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="imgModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="imgModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <img src="img/men_hoodie.jpg" data-dismiss="modal" aria-label="Close" />
         </div>
       </div>
     </div>
   </div>
-
 </template>
 <script>
 export default {
-    data() {
-        return {
-            id: this.$route.params.id,
-            product: {}
+  data() {
+    return {
+      id: this.$route.params.id,
+      product: {}
+    };
+  },
+  created() {
+    this.getProduct();
+  },
+  methods: {
+    getProduct() {
+      axios.get("products/" + this.id).then(
+        response => {
+          let { data } = response;
+
+          this.product = data;
+        },
+        error => {
+          console.error(error);
         }
-    },
-    created() {
-        this.getProduct();
-    },
-    methods: {
-        getProduct() {
-          axios.get('products/' + this.id).then(response => {
-            let { data } = response;     
-            
-            this.product = data;
-          },
-            error => {
-            console.error(error)
-          })
-        }
+      );
     }
-}
+  }
+};
 </script>
