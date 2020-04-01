ba869aec22eaa254d6122fd195503be7716dc310
Kristian <krismo94@hotmail.com>
2020-04-01 14:25:55 +0200

    Endret på kontakt oss siden, lagt til et placeholder bilde for google maps, lagt til noen classes for kontakt oss pagen i classes.scss, og endret tlf og mail i footer

--- resources/js/components/Contact.vue
+++ resources/js/components/Contact.vue
@@ -1,3 +1,32 @@
 <template>
-    <p>Kontaktoss</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <h1 class="contact-header">Kontakt oss</h1>
+        <h5>
+          <i class="fas fa-store"></i> Åpningstider
+        </h5>
+        <p class="pl-4">Man-Fre: 10:00-16:30 | Lørdag: 12:00-15:00</p>
+        <h5>
+          <i class="fas fa-phone"></i> Telefon
+        </h5>
+        <p class="pl-4">90 62 41 76</p>
+        <h5>
+          <i class="fas fa-envelope"></i> E-post
+        </h5>
+        <p class="pl-4">anund.grivi@nenett.no | anund@grivi.no</p>
+        <h5>
+          <i class="fas fa-map-marker-alt"></i> Her holder vi til
+        </h5>
+        <p class="pl-4">
+          Bøgata 75
+          3800 Bø i Telemark
+        </p>
+      </div>
+      <!-- placeholder for google maps api -->
+      <div class="col-lg-6 col-md-6 col-sm-12 mb-5">
+        <img src="img/map.png" />
+      </div>
+    </div>
+  </div>
 </template>
