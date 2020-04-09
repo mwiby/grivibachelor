4deb59d8d3dd9e20d2b0edb81104b91e1f4484a0
mwiby <markuswb77@gmail.com>
2020-04-09 23:44:57 +0200

    updat DB rel

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
  
+/*
+ - Første route kall når man trykker mann eller dame
+ - Returnerer 6 produkter og kategorier for kjønnet
+*/
+Route::get('/clothes/{gender}', 'ClothesController@index');
+
+
+
 
 //Returnerer ALLE brands
 Route::get('/clothes/brands', 'BrandController@index');
@@ -14,18 +22,12 @@ Route::get('/clothes/brands', 'BrandController@index');
 //Returnerer bare brands som eksisterer i oppgitt kategori
 Route::get('/clothes/{gender}/category/{c_slug}/brands', 'BrandController@show');
 
-
-/*
- - Første route kall når man trykker mann eller dame
- - Returnerer 6 produkter og kategorier for kjønnet
-*/
-Route::get('/clothes/{gender}', 'ClothesController@index');
-
-// Returnerer produktene til kategori
+// Returnerer produktene til kategori med brands
 Route::get('/clothes/{gender}/category/{c_slug}', 'CategoryController@show');
 
 // Returnerer produktet til slug
 Route::get('/product/{slug}', 'ProductController@showOne');
+
 
 
 /*Returnerer flere utvalgte brands sine produkter
@@ -36,6 +38,7 @@ Route::get('/product/{slug}', 'ProductController@showOne');
 Route::get('/clothes/{gender}/category/{c_slug}/brands/{slug}', 'ProductController@show')
 ->where('slug', '.*');
 */
+
 
 // admin siden
 Route::group(['prefix' => 'admin'], function () {
